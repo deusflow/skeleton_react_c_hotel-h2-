3ff33ab14fd2b4359c2bd3077bba665bb878e5f6
Beate Louise Jordening <beate@city.dk>
2025-08-25 20:39:11 +0200

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./styles/tailwind.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+console.log("ENV", import.meta.env);
+console.log("VITE_API_URL", import.meta.env.VITE_API_URL);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
